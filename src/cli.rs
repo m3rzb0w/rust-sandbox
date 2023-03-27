59cf21c854fc911e36971bedf7d62fa1cb2085ea
m3rzb0w <80rN70c0D3@protonmail.com>
2023-03-27 00:46:03 +0000

    cli up

--- src/cli.rs
+++ src/cli.rs
@@ -4,13 +4,15 @@ use std::env;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
-    let command = args[1].clone();
-    let name = "Toto";
-
-    println!("Args: {:?}", command);
-
-    if command == "hello" {
-        println!("Hi {}, whats up?", name);
+    if args.len() > 1 {
+        let command: String = args[1].clone();
+        let name: &str = "Toto";
+        println!("Args: {:?}", command);
+        if command == "hello" {
+            println!("Hi {}, whats up?", name);
+        } else {
+            println!("What do you mean by {} ?", command);
+        }
     } else {
         println!("No args found!");
     }
